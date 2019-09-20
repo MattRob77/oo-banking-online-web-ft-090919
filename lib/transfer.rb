b8a42c0cfba81b2c5f30c876df40ce2e72a2b697
Matthew Robinson <matt_robinson1@yahoo.com>
2019-09-20 11:50:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,16 +14,17 @@ class Transfer
   end 
   
   def execute_transaction
-        if valid? && @status == "pending" && sender.balance > amount
-        sender.balance -= amount 
-        receiver.balance += amount
-        @status = "complete" 
+    if valid? && @status == "pending" && sender.balance > amount
+      sender.balance -= amount 
+      receiver.balance += amount
+      @status = "complete"
       else
         @status = "rejected"
         "Transaction rejected. Please check your account balance."
       end
     end
   end 
+end 
 
 
 
